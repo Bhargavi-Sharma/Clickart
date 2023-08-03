246e39eff422391841b30046856b4903b5cde090
Bhargavi <bhargavi131313@gmail.com>
2023-08-03 23:30:00 +0530

    CHANGE

--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 import * as actionTypes from "../constants/product-constant";
 
-const URL = 'http://localhost:8000';
+// const URL = 'http://localhost:8000';
 
 export const getProducts = () => async (dispatch) => {
   try {
-    const { data } = await axios.get(`${URL}/products`);
-
+    const { data } = await axios.get(`${process.env.REACT_APP_API_URL}/products`);
     dispatch({ type: actionTypes.GET_PRODUCTS_SUCCESS, payload: data });
+    console.log(process.env.REACT_APP_API_URL);
   } catch (error) {
     dispatch({ type: actionTypes.GET_PRODUCTS_FAIL, payload: error.message });
   }
@@ -16,7 +16,7 @@ export const getProducts = () => async (dispatch) => {
 export const getProductDetails = (id) => async (dispatch) => {
   try {
     dispatch({ type: actionTypes.GET_PRODUCT_DETAILS_REQUEST });
-    const { data } = await axios.get(`${URL}/product/${id}`);
+    const { data } = await axios.get(`${process.env.REACT_APP_API_URL}/products/${id}`);
     dispatch({ type: actionTypes.GET_PRODUCT_DETAILS_SUCCESS, payload: data });
   } catch (error) {
     dispatch({
