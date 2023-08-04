828a615cc22c4950860bc0abcf9ff7ffe77dbde7
Bhargavi-Sharma <85413720+Bhargavi-Sharma@users.noreply.github.com>
2023-08-05 01:04:24 +0530

    Update footer.jsx

--- client/src/components/Footer/footer.jsx
+++ client/src/components/Footer/footer.jsx
@@ -1,10 +1,10 @@
-import {Box, Typography, styled} from '@mui/material';
+import {Grid, Typography, styled} from '@mui/material';
 import EmailIcon from '@mui/icons-material/Email';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import PersonIcon from '@mui/icons-material/Person';
 
 
-const Container= styled(Box)`
+const Container= styled(Grid)`
     background: #000000;
     height: 12vh;
     color: #fff;
@@ -13,7 +13,7 @@ const Container= styled(Box)`
 
 const Foot = () =>{ 
     return(
-        <Container style={{display: 'flex', marginTop: 'auto'}}>
+       <Container container item lg={12} ms={12} sm={12} xs={12} style={{display: 'flex', marginTop: 'auto', width: '100%'}}>
             <Typography style={{padding:"auto", margin:"auto"}}><PersonIcon style={{fontSize:'15',paddingTop:'3', marginTop:'0', marginRight:'2'}}/>&nbsp;Creator: Bhargavi</Typography>
             <Typography style={{padding:"auto", margin:"auto"}}><EmailIcon style={{fontSize:'15',paddingTop:'3', marginTop:'0', marginRight:'2'}}/>&nbsp;Contact: bhargavi131313@gmail.com</Typography>
             <Typography style={{padding:"auto", margin:"auto"}}><LinkedInIcon style={{fontSize:'15',paddingTop:'3', marginTop:'0', marginRight:'2'}}/>&nbsp;LinkedIn: linkedin.in/bhargavi-sharma</Typography>
